6b9ce9c4d74cbacae3c2c750a4b2f34a7ebbf0ec
Gaurav Tripathi <ramantripathi22@gmail.com>
2025-01-26 15:10:38 +0530

    login/signup form

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header relative z-10 bg-gradient-to-b from-black">
+      <div className='px-8 py-2'>
+          <img 
+              width="150" height="40" 
+              src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='Netflix Logo' />
+      </div>
+      <div className='head-nav'>
+        <ul>
+          <li>Language</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  }
   return (
-    <div>Login</div>
+    <div className='main-container'>
+      <Header />
+      <div className='background-nf'></div>
+      <div className='form-container'>   
+        <h1>{isSignInForm ? 'Sign In' : "Sign Up"}</h1>     
+        <form className='login-container'>
+          {!isSignInForm && ( 
+            <input 
+              type='text' 
+              placeholder='Full Name' 
+              className='form-control' 
+            />
+          )}          
+          <input type='text' placeholder='Email' className='form-control' />
+          <input type='password' placeholder='Password' className='form-control' />
+          <button className='btn mt-7'>{isSignInForm ? 'Sign In' : "Sign Up"}</button>
+          <p className=' mt-6 cursor-pointer' onClick={toggleSignInForm}>
+            {isSignInForm ? 
+              "New to Netflix? Sign Up Now" : 
+              "Already registered? Sign In Now"}
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
 
